1344b79c1a7b178e5ea14010b52804ca03c246dd
kd1186940 <kd1186940@st.kobedenshi.ac.jp>
2018-11-22 09:34:05 +0900

    test

--- src/pages/User/Present/Create/Create.vue
+++ src/pages/User/Present/Create/Create.vue
@@ -7,280 +7,241 @@
 
       <div class="main">
 
+        <form
+          @submit.prevent="handleSubmit"
+          class="w__form"
+        >
+
         <div class="cp_ipselect">
-          <select class="cp_sl06" required>
-          <option value="" hidden disabled selected></option>
-          <option value="1">travel okinawa</option>
-          <option value="2">travel kyoto</option>
-          <option value="3">travel 上司</option>
-          <option value="4">travel 親戚</option>
+          <select class="cp_sl06" required v-model="article_id">
+            <option value="" hidden disabled selected></option>
+            <option value="item1">item1</option>
+            <option value="item2">item2</option>
+            <option value="item3">item3</option>
+            <option value="item4">item4</option>
           </select>
           <span class="cp_sl06_highlight"></span>
           <span class="cp_sl06_selectbar"></span>
         <label class="cp_sl06_selectlabel">記事選択</label>
         </div>
 
-        <div class="present" v-show="form3">
+          <div class="present" v-if="form3">
 
-        <div class="w__field mt20mb20">
-          <label>商品名</label>
-          <div class="input">
-            <input
-              type="text"
-              name="title"
-            />
-          </div>
-          <div
-            class="form__errors"
-            v-if="errors.has('title')"
-          >
-            商品名は必須です
-          </div>
-        </div>
-
-        <div class="amount">
-          <label>個数</label>
-          <div class="input">
-            <input
-              type="text"
-              name="amount"
-            />
-          </div>
-          <div
-            class="form__errors"
-            v-if="errors.has('amount')"
-          >
-            個数は必須です
-          </div>
-        </div>
-
-        <div class="price">
-          <label>価格</label>
-          <div class="input">
-            <input
-              type="text"
-              name="price"
-            />
-          </div>
-          <div
-            class="form__errors"
-            v-if="errors.has('price')"
-          >
-            価格は必須です
-          </div>
-        </div>
-
-        <div class="cp_ipselect">
-          <select class="cp_sl06" required>
-            <option value="" hidden disabled selected></option>
-            <option value="1">両親</option>
-            <option value="2">友達</option>
-            <option value="3">上司</option>
-            <option value="4">親戚</option>
-          </select>
-          <span class="cp_sl06_highlight"></span>
-          <span class="cp_sl06_selectbar"></span>
-        <label class="cp_sl06_selectlabel">送る相手</label>
-        </div>
-
-        <div class="w__field mt20mb20">
-          <label>感想</label>
-          <div class="input">
-            <textarea
-              name="content"
-            />
-          </div>
-          <div
-            class="form__error"
-            v-if="errors.has('content')"
-          >
-            感想は必須です
-          </div>
-        </div>
-
-        <div class="file">
-          ファイルの選択
-          <input type="file" v-on:change="onFileChange3">
-        </div>
-        <img class="showimg" v-show="uploadedImage3" :src="uploadedImage3" />
-      </div>
-        <div class="present" v-show="form2">
-
-          <div class="w__field mt20mb20">
-            <label>商品名</label>
-            <div class="input">
-             <input
-               type="text"
-               name="title"
-             />
+            <div class="w__field mt20mb20">
+              <label>商品名</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_name3"
+                  required
+                  v-model.trim="presents[2].present_name"
+                />
+              </div>
             </div>
-            <div
-             class="form__errors"
-             v-if="errors.has('title')"
-            >
-             商品名は必須です
+
+            <div class="amount">
+              <label>個数</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_amount3"
+                  required
+                  v-model.trim="presents[2].present_amount"
+                />
+              </div>
+            </div>
+
+            <div class="price">
+              <label>価格</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_price3"
+                  required
+                  v-model.trim="presents[2].present_price"
+                />
+              </div>
+            </div>
+
+            <div class="cp_ipselect">
+              <select class="cp_sl06" required v-model="presents[2].required">
+                <option value="" hidden disabled selected></option>
+                <option value="両親">両親</option>
+                <option value="友達">友達</option>
+                <option value="上司">上司</option>
+                <option value="親戚">親戚</option>
+              </select>
+              <span class="cp_sl06_highlight"></span>
+              <span class="cp_sl06_selectbar"></span>
+            <label class="cp_sl06_selectlabel">送る相手</label>
+            </div>
+
+            <div class="w__field mt20mb20">
+              <label>感想</label>
+              <div class="input">
+                <textarea
+                  name="impression3"
+                  required
+                  v-model.trim="presents[2].impression"
+                />
+              </div>
+            </div>
+
+            <div class="file">
+              ファイルの選択
+              <input type="file" v-on:change="onFileChange1">
+            </div>
+            <img class="showimg" v-show="presents[2].photo" :src="presents[2].photo" />
+
           </div>
-        </div>
 
-        <div class="amount">
-          <label>個数</label>
-          <div class="input">
-            <input
-              type="text"
-              name="amount"
-            />
+          <div class="present" v-if="form2">
+
+            <div class="w__field mt20mb20">
+              <label>商品名</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_name2"
+                  required
+                  v-model.trim="presents[1].present_name"
+                />
+              </div>
+            </div>
+
+            <div class="amount">
+              <label>個数</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_amount2"
+                  required
+                  v-model.trim="presents[1].present_amount"
+                />
+              </div>
+            </div>
+
+            <div class="price">
+              <label>価格</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_price2"
+                  required
+                  v-model.trim="presents[1].present_price"
+                />
+              </div>
+            </div>
+
+            <div class="cp_ipselect">
+              <select class="cp_sl06" required v-model="presents[1].required">
+                <option value="" hidden disabled selected></option>
+                <option value="両親">両親</option>
+                <option value="友達">友達</option>
+                <option value="上司">上司</option>
+                <option value="親戚">親戚</option>
+              </select>
+              <span class="cp_sl06_highlight"></span>
+              <span class="cp_sl06_selectbar"></span>
+            <label class="cp_sl06_selectlabel">送る相手</label>
+            </div>
+
+            <div class="w__field mt20mb20">
+              <label>感想</label>
+              <div class="input">
+                <textarea
+                  name="impression2"
+                  required
+                  v-model.trim="presents[1].impression"
+                />
+              </div>
+            </div>
+
+            <div class="file">
+              ファイルの選択
+              <input type="file" v-on:change="onFileChange2">
+            </div>
+            <img class="showimg" v-show="presents[1].photo" :src="presents[1].photo" />
+
           </div>
-          <div
-            class="form__errors"
-            v-if="errors.has('amount')"
-          >
-            個数は必須です
+
+          <div class="present">
+
+            <div class="w__field mt20mb20">
+              <label>商品名</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_name"
+                  required
+                  v-model.trim="presents[0].present_name"
+                />
+              </div>
+            </div>
+
+            <div class="amount">
+              <label>個数</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_amount"
+                  required
+                  v-model.trim="presents[0].present_amount"
+                />
+              </div>
+            </div>
+
+            <div class="price">
+              <label>価格</label>
+              <div class="input">
+                <input
+                  type="text"
+                  name="present_price"
+                  required
+                  v-model.trim="presents[0].present_price"
+                />
+              </div>
+            </div>
+
+            <div class="cp_ipselect">
+              <select class="cp_sl06" required v-model="presents[0].required">
+                <option value="" hidden disabled selected></option>
+                <option value="両親">両親</option>
+                <option value="友達">友達</option>
+                <option value="上司">上司</option>
+                <option value="親戚">親戚</option>
+              </select>
+              <span class="cp_sl06_highlight"></span>
+              <span class="cp_sl06_selectbar"></span>
+            <label class="cp_sl06_selectlabel">送る相手</label>
+            </div>
+
+            <div class="w__field mt20mb20">
+              <label>感想</label>
+              <div class="input">
+                <textarea
+                  required
+                  name="impression"
+                  v-model.trim="presents[0].impression"
+                />
+              </div>
+            </div>
+
+            <div class="file">
+              ファイルの選択
+              <input type="file" v-on:change="onFileChange1">
+            </div>
+            <img class="showimg" v-show="presents[0].photo" :src="presents[0].photo" />
+
+            <div class="actionBtn">
+              <input
+                type="submit"
+                value="投稿する"
+              />
+            </div>
+
           </div>
-        </div>
-
-        <div class="price">
-          <label>価格</label>
-          <div class="input">
-            <input
-              type="text"
-              name="price"
-            />
-          </div>
-          <div
-            class="form__errors"
-            v-if="errors.has('price')"
-          >
-            価格は必須です
-          </div>
-        </div>
-
-        <div class="cp_ipselect">
-          <select class="cp_sl06" required>
-          <option value="" hidden disabled selected></option>
-          <option value="1">両親</option>
-          <option value="2">友達</option>
-          <option value="3">上司</option>
-          <option value="4">親戚</option>
-          </select>
-          <span class="cp_sl06_highlight"></span>
-          <span class="cp_sl06_selectbar"></span>
-        <label class="cp_sl06_selectlabel">送る相手</label>
-        </div>
-
-        <div class="w__field mt20mb20">
-          <label>感想</label>
-          <div class="input">
-            <textarea
-              name="content"
-            />
-          </div>
-          <div
-            class="form__error"
-            v-if="errors.has('content')"
-          >
-            感想は必須です
-          </div>
-        </div>
-
-        <div class="file">
-          ファイルの選択
-          <input type="file" v-on:change="onFileChange2">
-        </div>
-        <img class="showimg" v-show="uploadedImage2" :src="uploadedImage2" />
-      </div>
-
-      <div class="present">
-
-      <div class="w__field mt20mb20">
-        <label>商品名</label>
-        <div class="input">
-          <input
-            type="text"
-            name="title"
-          />
-        </div>
-        <div
-          class="form__errors"
-          v-if="errors.has('title')"
-        >
-          商品名は必須です
-        </div>
-      </div>
-
-      <div class="amount">
-        <label>個数</label>
-        <div class="input">
-          <input
-            type="text"
-            name="amount"
-          />
-        </div>
-        <div
-          class="form__errors"
-          v-if="errors.has('amount')"
-        >
-          個数は必須です
-        </div>
-      </div>
-
-      <div class="price">
-        <label>価格</label>
-        <div class="input">
-          <input
-            type="text"
-            name="price"
-          />
-        </div>
-        <div
-          class="form__errors"
-          v-if="errors.has('price')"
-        >
-          価格は必須です
-        </div>
-      </div>
-
-      <div class="cp_ipselect">
-        <select class="cp_sl06" required>
-        <option value="" hidden disabled selected></option>
-        <option value="1">両親</option>
-        <option value="2">友達</option>
-        <option value="3">上司</option>
-        <option value="4">親戚</option>
-        </select>
-        <span class="cp_sl06_highlight"></span>
-        <span class="cp_sl06_selectbar"></span>
-      <label class="cp_sl06_selectlabel">送る相手</label>
-      </div>
-
-      <div class="w__field mt20mb20">
-        <label>感想</label>
-        <div class="input">
-          <textarea
-            name="content"
-          />
-        </div>
-        <div
-          class="form__error"
-          v-if="errors.has('content')"
-        >
-          感想は必須です
-        </div>
-      </div>
-
-      <div class="file">
-        ファイルの選択
-        <input type="file" v-on:change="onFileChange1">
-      </div>
-      <img class="showimg" v-show="uploadedImage1" :src="uploadedImage1" />
-
-      <div class="actionBtn">
-        <input
-          type="submit"
-          value="投稿する"
-        />
-      </div>
-
-        </div>
+        </form>
       </div>
     </div>
   </div>
@@ -291,10 +252,33 @@ export default {
   name: 'CreatePresent',
   data () {
     return {
-      name: '',
-      uploadedImage1: '',
-      uploadedImage2: '',
-      uploadedImage3: '',
+      article_id: '',
+      presents: [
+        {
+          present_name: '',
+          present_amount: '',
+          present_price: '',
+          required: '',
+          impression: '',
+          photo: ''
+        },
+        {
+          present_name: '',
+          present_amount: '',
+          present_price: '',
+          required: '',
+          impression: '',
+          photo: ''
+        },
+        {
+          present_name: '',
+          present_amount: '',
+          present_price: '',
+          required: '',
+          impression: '',
+          photo: ''
+        }
+      ],
       form2: false,
       form3: false
     }
@@ -307,7 +291,7 @@ export default {
     createImage1 (file) {
       let reader = new FileReader()
       reader.onload = e => {
-        this.uploadedImage1 = e.target.result
+        this.presents[0].photo = e.target.result
       }
       reader.readAsDataURL(file)
     },
@@ -318,7 +302,7 @@ export default {
     createImage2 (file) {
       let reader = new FileReader()
       reader.onload = e => {
-        this.uploadedImage2 = e.target.result
+        this.presents[1].photo = e.target.result
       }
       reader.readAsDataURL(file)
     },
@@ -329,7 +313,7 @@ export default {
     createImage3 (file) {
       let reader = new FileReader()
       reader.onload = e => {
-        this.uploadedImage3 = e.target.result
+        this.presents[0].photo = e.target.result
       }
       reader.readAsDataURL(file)
     },
@@ -346,6 +330,9 @@ export default {
       } else if (this.form2 === true && this.form3 === false) {
         this.form2 = false
       }
+    },
+    handleSubmit () {
+      console.log(JSON.stringify(this.presents) + JSON.stringify(this.article_id))
     }
   }
 }
