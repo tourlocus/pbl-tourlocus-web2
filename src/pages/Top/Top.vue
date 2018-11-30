59a8ea6c5a4f31b7a2d8a30a65251b13357175b7
hortensian <kd1196544@st.kobedenshi.ac.jp>
2018-11-30 11:10:03 +0900

    [change]記事の一覧をカード型に

--- src/pages/Top/Top.vue
+++ src/pages/Top/Top.vue
@@ -7,10 +7,21 @@
           <a href="#" class="square_btn">人気順</a>
         </div>
 
-        <div class="memo">
-          <span class="masking-tape">
-          </span><p>おきなわ</p>
-        </div>
+        <section class="card">
+          <img class="card-img" src="../../assets/photo1.jpg" alt="">
+          <div class="card-content">
+            <img class="card-icon" src="../../assets/icon1.png">
+            <div class="username">
+              {{username}}
+            </div>
+            <h1 class="card-title">{{title}}</h1>
+            <p class="card-text">{{content}}</p>
+          </div>
+          <div class="card-link">
+            <a href="#"></a>
+            <a href="#">もっと見る</a>
+          </div>
+        </section>
 
         <section class="card">
           <img class="card-img" src="../../assets/photo1.jpg" alt="">
@@ -19,14 +30,47 @@
             <div class="username">
               {{username}}
             </div>
-            <h1 class="card-title">沖縄に行ってきた</h1>
-            <p class="card-text">沖縄といえばやはり海なのですが、今回私は海ではなく、公園にフォーカスを当ててみました</p>
+            <h1 class="card-title">{{title}}2</h1>
+            <p class="card-text">content</p>
           </div>
           <div class="card-link">
             <a href="#"></a>
             <a href="#">もっと見る</a>
           </div>
         </section>
+
+        <section class="card">
+          <img class="card-img" src="../../assets/photo1.jpg" alt="">
+          <div class="card-content">
+            <img class="card-icon" src="../../assets/icon1.png">
+            <div class="username">
+              {{username}}
+            </div>
+            <h1 class="card-title">{{title}}3</h1>
+            <p class="card-text" id="lengthcheck">{{content}}</p>
+          </div>
+          <div class="card-link">
+            <a href="#"></a>
+            <a href="#">もっと見る</a>
+          </div>
+        </section>
+
+        <section class="card">
+          <img class="card-img" src="../../assets/photo1.jpg" alt="">
+          <div class="card-content">
+            <img class="card-icon" src="../../assets/icon1.png">
+            <div class="username">
+              {{username}}
+            </div>
+            <h1 class="card-title">{{title}}4</h1>
+            <p class="card-text">{{content}}</p>
+          </div>
+          <div class="card-link">
+            <a href="#"></a>
+            <a href="#">もっと見る</a>
+          </div>
+        </section>
+
       </div>
     </div>
   </div>
@@ -44,12 +88,21 @@ export default {
   },
   data () {
     return {
+      articles: [
+        {
+        userid: '',
+        username: '',
+        icon: '',
+        title: '沖縄に行ってきた',
+        content: '海外旅行ではないのでパスポートもいらず、安価で南国の雰囲気を満喫することができます。そんな魅力ある沖縄旅行に行ってきました！今回は、実際に行った沖縄旅行の体験談について詳しく紹介していきます！沖縄旅行の参考にしてみてください♪'
+        }
+        ],
       username: '@hogehoge',
+      title: '沖縄に行ってきた',
       content: '海外旅行ではないのでパスポートもいらず、安価で南国の雰囲気を満喫することができます。そんな魅力ある沖縄旅行に行ってきました！今回は、実際に行った沖縄旅行の体験談について詳しく紹介していきます！沖縄旅行の参考にしてみてください♪'
     }
     },
   methods: {
-
   }
 }
 </script>
