b8359dee67cad989d0a03c534566858756972bd0
kd1186940 <kd1186940@st.kobedenshi.ac.jp>
2019-01-11 14:33:12 +0900

    [wip]

--- src/pages/User/Present/Edit/Edit.vue
+++ src/pages/User/Present/Edit/Edit.vue
@@ -1,185 +1,20 @@
 <template>
   <div class="p-edit-present">
     <div class="container">
-
-    <button @click="addmain" class="addmain">+</button>
-    <button @click="deletemain" class="deletemain">-</button>
-
       <div class="main">
 
-        <form
-          @submit.prevent="handleSubmit"
-          class="w__form"
-        >
-
-          <div class="cp_ipselect">
-          <select class="cp_sl06" v-model="article_id">
-            <option value="" hidden disabled selected></option>
-            <option value="記事1">記事1</option>
-            <option value="記事2">記事2</option>
-            <option value="記事3">記事3</option>
-            <option value="記事4">記事4</option>
-          </select>
-          <span class="cp_sl06_highlight"></span>
-          <span class="cp_sl06_selectbar"></span>
-        <label class="cp_sl06_selectlabel">記事選択</label>
-        </div>
-        <div class="present" v-if="form3">
-
-          <img class="showimg" v-show="image3" :src="image3" />
-          <div class="file">
-            写真の選択
-            <input type="file" v-on:change="onFileChange3">
-          </div>
-
-          <div class="cp_ipselect">
-            <select class="cp_sl06" omiyage v-model="presents[2].present_type">
-              <option value="" hidden disabled selected></option>
-              <option value="洋菓子">洋菓子</option>
-              <option value="和菓子">和菓子</option>
-              <option value="キーホルダー">キーホルダー</option>
-              <option value="人形">人形</option>
-              <option value="飲料">飲料</option>
-              <option value="その他">その他</option>
-            </select>
-            <span class="cp_sl06_highlight"></span>
-            <span class="cp_sl06_selectbar"></span>
-            <label class="cp_sl06_selectlabel">種類</label>
-          </div>
-
-          <div class="cp_ipselect">
-            <select class="cp_sl06" required v-model="presents[2].required">
-              <option value="" hidden disabled selected></option>
-              <option value="両親">両親</option>
-              <option value="友達">友達</option>
-              <option value="上司">上司</option>
-              <option value="親戚">親戚</option>
-            </select>
-            <span class="cp_sl06_highlight"></span>
-            <span class="cp_sl06_selectbar"></span>
-          <label class="cp_sl06_selectlabel">送る相手</label>
-          </div>
-
-          <div class="amount">
-            <label>人数</label>
-            <div class="input">
-              <input
-                type="number"
-                name="present_amount3"
-                required
-                v-model.number="presents[2].present_amount"
-              />人
-            </div>
-          </div>
-
-          <div class="price">
-            <label>単価</label>
-            <div class="input">
-              <input
-                type="number"
-                name="present_price3"
-                required
-                v-model.number="presents[2].present_price"
-              />円
-            </div>
-          </div>
-
-          <div class="w__field mt20mb20">
-            <label>感想</label>
-            <div class="input">
-              <textarea
-                name="impression3"
-                required
-                v-model.trim="presents[2].impression"
-              />
-            </div>
-          </div>
-
-        </div>
-
-        <div class="present" v-if="form2">
-
-          <img class="showimg" v-show="image2" :src="image2" />
-          <div class="file">
-            写真の選択
-            <input type="file" v-on:change="onFileChange2">
-          </div>
-
-          <div class="cp_ipselect">
-            <select class="cp_sl06" omiyage v-model="presents[1].present_type">
-              <option value="" hidden disabled selected></option>
-              <option value="洋菓子">洋菓子</option>
-              <option value="和菓子">和菓子</option>
-              <option value="キーホルダー">キーホルダー</option>
-              <option value="人形">人形</option>
-              <option value="飲料">飲料</option>
-              <option value="その他">その他</option>
-            </select>
-            <span class="cp_sl06_highlight"></span>
-            <span class="cp_sl06_selectbar"></span>
-            <label class="cp_sl06_selectlabel">種類</label>
-          </div>
-
-          <div class="cp_ipselect">
-            <select class="cp_sl06" required v-model="presents[1].required">
-              <option value="" hidden disabled selected></option>
-              <option value="両親">両親</option>
-              <option value="友達">友達</option>
-              <option value="上司">上司</option>
-              <option value="親戚">親戚</option>
-            </select>
-            <span class="cp_sl06_highlight"></span>
-            <span class="cp_sl06_selectbar"></span>
-          <label class="cp_sl06_selectlabel">送る相手</label>
-          </div>
-
-          <div class="amount">
-            <label>人数</label>
-            <div class="input">
-              <input
-                type="number"
-                name="present_amount2"
-                required
-                v-model.number="presents[1].present_amount"
-              />人
-            </div>
-          </div>
-
-          <div class="price">
-            <label>単価</label>
-            <div class="input">
-              <input
-                type="number"
-                name="present_price2"
-                required
-                v-model.number="presents[1].present_price"
-              />円
-            </div>
-          </div>
-
-          <div class="w__field mt20mb20">
-            <label>感想</label>
-            <div class="input">
-              <textarea
-                name="impression2"
-                required
-                v-model.trim="presents[1].impression"
-              />
-            </div>
-          </div>
-
-        </div>
+        <form @submit.prevent="handleSubmit">
 
         <div class="present">
 
-          <img class="showimg" v-show="image1" :src="image1" />
+          <img class="showimg" v-show="image" :src="image" />
           <div class="file">
             写真の選択
-            <input type="file" v-on:change="onFileChange1">
+            <input type="file" v-on:change="onFileChange">
           </div>
 
           <div class="cp_ipselect">
-            <select class="cp_sl06" omiyage v-model="presents[0].present_type">
+            <select required class="cp_sl06" omiyage v-model="presents.present_name">
               <option value="" hidden disabled selected></option>
               <option value="洋菓子">洋菓子</option>
               <option value="和菓子">和菓子</option>
@@ -194,7 +29,7 @@
           </div>
 
           <div class="cp_ipselect">
-            <select class="cp_sl06" required v-model="presents[0].required">
+            <select class="cp_sl06" required v-model="presents.required">
               <option value="" hidden disabled selected></option>
               <option value="両親">両親</option>
               <option value="友達">友達</option>
@@ -213,7 +48,7 @@
                 type="number"
                 name="present_amount"
                 required
-                v-model.number="presents[0].present_amount"
+                v-model.number="presents.present_amount"
               />人
             </div>
           </div>
@@ -225,7 +60,7 @@
                 type="number"
                 name="present_price"
                 required
-                v-model.number="presents[0].present_price"
+                v-model.number="presents.present_price"
               />円
             </div>
           </div>
@@ -236,7 +71,8 @@
               <textarea
                 required
                 name="impression"
-                v-model.trim="presents[0].impression"
+                maxlength="255"
+                v-model.trim="presents.impression"
               />
             </div>
           </div>
@@ -250,190 +86,112 @@
           </div>
 
         </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 import axios from 'axios'
+import {Sleep} from '../../../../utils'
 
 export default {
   name: 'EditPresent',
   data () {
     return {
-      isLoading: false,
-      article_name: '記事a',
-      article_id: '1',
-      presents: [
-        {
-          present_type: '洋菓子',
-          present_amount: '1',
-          present_price: '1',
-          required: '両親',
-          impression: 'impression1',
-          photo: null
-        },
-        {
-          present_type: '洋菓子',
-          present_amount: '2',
-          present_price: '2',
-          required: '友達',
-          impression: 'impression2',
-          photo: null
-        },
-        {
-          present_type: '洋菓子',
-          present_amount: '3',
-          present_price: '3',
-          required: '親戚',
-          impression: 'impression3',
-          photo: null
-        }
-      ],
-      form2: false,
-      form3: false,
-      image1: '',
-      image2: '',
-      image3: ''
+      article_id: '',
+      article_name: '',
+      presents: {
+        present_name: '',
+        present_amount: '',
+        present_price: '',
+        required: '',
+        impression: '',
+        photo: null
+      },
+      image: '',
+      isLoading: false
     }
   },
   methods: {
     updateIsLoading (v) {
       this.isLoading = v
     },
-    onFileChange1: function (e) {
+    updateIsSubmitLoading (v) {
+      this.isSubmitLoading = v
+    },
+    onFileChange: function (e) {
       e.preventDefault()
       let files = e.target.files
-      this.presents[0].photo = files[0]
-      this.createImage1(files[0])
+      this.presents.photo = files[0]
+      this.createImage(files[0])
     },
-    createImage1 (file) {
+    createImage (file) {
       let reader = new FileReader()
       reader.onload = e => {
-        this.image1 = e.target.result
+        this.image = e.target.result
       }
       reader.readAsDataURL(file)
-    },
-    onFileChange2: function (e) {
-      e.preventDefault()
-      let files = e.target.files
-      this.presents[1].photo = files[0]
-      this.createImage2(files[0])
-    },
-    createImage2 (file) {
-      let reader = new FileReader()
-      reader.onload = e => {
-        this.image2 = e.target.result
-      }
-      reader.readAsDataURL(file)
-    },
-    onFileChange3: function (e) {
-      e.preventDefault()
-      let files = e.target.files
-      this.presents[2].photo = files[0]
-      this.createImage3(files[0])
-    },
-    createImage3 (file) {
-      let reader = new FileReader()
-      reader.onload = e => {
-        this.image3 = e.target.result
-      }
-      reader.readAsDataURL(file)
-    },
-    addmain () {
-      if (this.form2 === false && this.form3 === false) {
-        this.form2 = true
-      } else if (this.form2 === true && this.form3 === false) {
-        this.form3 = true
-      }
-    },
-    deletemain () {
-      if (this.form2 === true && this.form3 === true) {
-        this.form3 = false
-      } else if (this.form2 === true && this.form3 === false) {
-        this.form2 = false
-      }
     },
     handleSubmit () {
-      this.updateIsLoading(true)
       const form = new FormData()
       form.append('article_id', this.article_id)
-      form.append('photo1', this.presents[0].photo)
-      form.append('photo2', this.presents[1].photo)
-      form.append('photo3', this.presents[2].photo)
+      form.append('present_name', this.presents.present_name)
+      form.append('present_amount', this.presents.present_amount)
+      form.append('present_price', this.presents.present_price)
+      form.append('required', this.presents.required)
+      form.append('impression', this.presents.impression)
+      form.append('photo', this.presents.photo)
 
-      var status
-
-      axios.post('http://localhost:3000/presents/create', {
-        presents: [
-          {
-            present_name: this.presents[0].present_type,
-            present_amount: this.presents[0].present_amount,
-            present_price: this.presents[0].present_price,
-            required: this.presents[0].required,
-            impression: this.presents[0].impression,
-            image: "image1",
-            impression: this.presents[0].impression
-          },
-          {
-            present_name: this.presents[1].present_type,
-            present_amount: this.presents[1].present_amount,
-            present_price: this.presents[1].present_price,
-            required: this.presents[1].required,
-            impression: this.presents[1].impression,
-            image: "image2",
-            impression: this.presents[1].impression
-          },
-          {
-            present_name: this.presents[2].present_type,
-            present_amount: this.presents[2].present_amount,
-            present_price: this.presents[2].present_price,
-            required: this.presents[2].required,
-            impression: this.presents[2].impression,
-            image: "image3",
-            impression: this.presents[2].impression
-          }
-        ],
-        article_id: this.article_id
-      }
-      ).then(response => {
-        axios.post('http://localhost:3000/presents/create_image', form).then(response =>
-          {this.updateIsLoading(false)}).cach(err =>
-            {this.updateIsLoading(false)
-              console.log('err:', err)})
-      }).catch(err => {
-        this.updateIsLoading(false)
-        console.log('err:', err)
+      this.$validator.validateAll().then(async result => {
+        if (result) {
+          this.updateIsSubmitLoading(true)
+          await axios({
+            url: `http://localhost:3000/presents/update`,
+            method: 'PUT',
+            data: form
+          })
+          .then(() => {
+            this.$message('更新しました。')
+            this.updateIsSubmitLoading(false)
+          })
+          .catch(() => {
+            this.$message('更新できませんでした。')
+            this.updateIsSubmitLoading(false)
+          })
+        }
       })
     }
   },
-  created: function() {
-    axios.get('http://localhost:3000/presents/edit/10')
-    .then((res) => {
-      this.article_id = res.data.article_id
-      for (var i = 0; i<3; i++){
-        if (res.data.presents[i]){
-          if (i == 0) this.image1 = require(`${res.data.presents[i].photo}`)
-          if (i == 1) {
-            this.form2 = true
-            this.image2 = res.data.presents[i].photo
-            console.log(res.data.presents[i].photo)
-            this.test(res, i)
-          }
-          if (i == 2) {
-            this.form3 = true
-            this.image3 = res.data.presents[i].photo
-          }
-          this.presents[i].present_name = res.data.presents[i].present_name
-          this.presents[i].present_amount = res.data.presents[i].present_amount
-          this.presents[i].present_price = res.data.presents[i].present_price
-          this.presents[i].required = res.data.presents[i].required
-          this.presents[i].impression = res.data.presents[i].impression
-        }
-      }
+  async created (credential) {
+    this.updateIsLoading(true)
+    await axios({
+      url: `http://localhost:3000/presents/edit/${this.$route.params.id}`,
+      method: 'GET',
+      headers: {
+        client: credential.client,
+        uid: credential.uid,
+        'access-token': credential.accessToken,
+        'Content-Type': 'multipart/form-data'
+      },
     })
+      .then(async res => {
+        this.presents.present_name = res.data.present_name
+        this.presents.present_amount = res.data.present_amount
+        this.presents.present_price = res.data.present_price
+        this.presents.required = res.data.required
+        this.presents.impression = res.data.impression
+        this.image = 'http://localhost:3000/' + res.data.photo
+        this.article_id = res.data.article_id
+        this.article_name = res.data.article_name
+        this.updateIsLoading(false)
+      })
+      .catch(async () => {
+        await Sleep(2000)
+        this.updateIsLoading(false)
+        this.$message('記事を取得できませんでした')
+      })
   }
 }
 </script>
